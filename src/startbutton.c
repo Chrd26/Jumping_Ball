ad41a22b877fd934c80bd89ec1f21719d664cdfc
Chrd26 <christosdouris@outlook.com>
2024-05-12 20:00:36 +0300

    Started working on the start button

--- src/startbutton.c
+++ src/startbutton.c
@@ -1,1 +1,20 @@
+#include "startbutton.h"
 
+void GenerateButton(TTF_Font *font, SDL_Renderer *renderer, struct StartButton &startButton, bool isHovering)
+{
+	SDL_Color color = {0xFF, 0xFF, 0xFF, 0xFF};
+	SDL_Surface *buttonTextSurface = TTF_RenderText_Solid(font, "Start", color);
+	
+	if (startButton.width == 0 || startButton.height == 0)
+	{
+		startButton.width = buttonTextSurface->w;
+		startButton.height = buttonTextSurface->height; 
+	}
+	
+	SDL_Texture *buttonTextTexture = SDL_CreateTextureFromSurface(renderer, buttonTextSurface);
+	const SDL_FRect buttonTextHolder = {startButton.x, startButton.y, 
+																			startButton.width, startButton.height};
+																			
+	SDL_RenderTexture(renderer, buttonTextTexture, NULL, &buttonTextHolder);
+	
+}
--- src/startbutton.h
+++ src/startbutton.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
+struct StartButton
+{
+	int x;
+	int y;
+	float width;
+	float height;
+};
+
+void GenerateButton(TTF_Font *font, SDL_Renderer *renderer, struct StartButton startButton, bool isHovering);
