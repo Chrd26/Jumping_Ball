0ff5d340469958bb7e94189f5e48ecec03cf7e95
Chrd26 <christosdouris@outlook.com>
2024-05-17 22:01:54 +0300

    Removed prints

--- src/results.c
+++ src/results.c
@@ -8,15 +8,11 @@ void GetResults(char *location, char *value)
 	char *commandPartSecond = "&& fpm run -- ";
 	char *command = calloc(	strlen(location) + strlen(value) + strlen(commandPartFirst) + strlen(commandPartSecond), 
 													sizeof(char));
-	printf("%s\n", command);
+													
 	strcpy(command, commandPartFirst);
-	printf("%s\n", command);
 	strcat(command, location);
-	printf("%s\n", command);
 	strcat(command, commandPartSecond);
-	printf("%s\n", command);
 	strcat(command, value);
-	printf("%s\n", command);
 	
 	int getSystemInfo = system(command);
 	printf("%d\n", getSystemInfo);
