026cb471f0c1f6ec0962f752455fda3f3ee0bd6c
Chris <christosdouris@outlook.com>
2024-07-10 17:54:52 +0300

    The application run on windows now but it's buggy, it needs fixes

---
+++ Jumping_Ball_VS/src/text.c
@@ -0,0 +1,59 @@
+#include "text.h"
+
+bool TextComponentInit(Resource *resource)
+{
+	int getValue = TTF_Init();
+	
+	if (getValue != 0)
+	{
+		printf("Failed to load TTF, %s", TTF_GetError());
+		return false;
+	}
+	
+	// load font
+	/*
+	char *temp = FindResource(resource->location);
+
+    for (int i = 0; i < strlen(temp); i++)
+    {
+        resource->location[i] = temp[i];
+    }
+
+    resource->location[strlen(temp)] = '\0';
+	*/
+	return true;
+}
+
+void DisplayText(SDL_Renderer * renderer, TTF_Font *font, char* content, int x, int y)
+{
+	SDL_Color textColor = {0xff, 0xff, 0xff};
+	SDL_Surface *textSurface = TTF_RenderText_Solid(font, content, textColor);
+	
+	if (textSurface == NULL)
+	{
+		printf("Failed to create surface: %s", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+	
+	SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+	
+	if (textTexture == NULL)
+	{
+		printf("Failed to create texture: %s", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+	
+	float width = textSurface->w;
+	float height = textSurface->h;
+	
+	const SDL_FRect textHolder = {x, y, width, height};
+	SDL_RenderTexture(renderer, textTexture, NULL, &textHolder);
+	
+	SDL_DestroySurface(textSurface);
+	textSurface = NULL;
+	
+	SDL_DestroyTexture(textTexture);
+	textTexture = NULL;
+}
+
+
